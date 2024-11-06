c31da6ed1939b8b9e4972e5a4432bd9def23d507
Nguyen Anh Vu <vunguyen.nav2004@gmail.com>
2024-11-06 16:39:45 +0700

    add BAN admin URL for Customer

--- src/main/java/com/onlineshop/controller/AdminController.java
+++ src/main/java/com/onlineshop/controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.onlineshop.controller;
 
+import com.onlineshop.entity.User;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,7 +11,14 @@ public class
 AdminController {
 
     @GetMapping("/admin")
-    public String homePage() {
+    public String homePage(HttpSession session, Model model) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        if(user.getRole().getId()==1){
+            return "redirect:/customer";
+        }
         return "adminHome";
     }
 }
